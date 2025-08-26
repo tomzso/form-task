116c986c4587097698e91207678c2eef7ef98644
tomzso <huntominuh@gmail.com>
2025-08-27 01:04:54 +0200

    improving ui

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,21 @@ import {
   Route,
 } from "react-router-dom";
 
-import { Navbar } from "./components/navbar/navbar";
 
 import { RenderForm } from "./pages/renderForm/renderForm";
 
-//import {DemoQuestionEditor} from "./components/pages/formBuilder/question/demoQuestionEditor"; 
-
-import { FormContextProvider } from "./context/form-context";
 
 function App() {
 
   return (
     <div className="App">
-      <FormContextProvider>
+
         <Router>
           <Routes>
             <Route path="*" element={<RenderForm/>} />
           </Routes>
         </Router>
-      </FormContextProvider>
+
     </div>
   );
 }
--- src/components/pages/renderForm/scoreDisplay/scoreDisplay.jsx
+++
@@ -1,9 +0,0 @@
-import "./scoreDisplay.css";
-
-export const ScoreDisplay = ({ score, maxScore }) => (
-  <div className="score-container">
-    <p>Your Score: {score} / {maxScore}</p>
-  </div>
-);
-
-
