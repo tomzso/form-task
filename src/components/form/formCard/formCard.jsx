b08c503e1e14e77df0e63a2557139a7139ae05e2
tomzso <huntominuh@gmail.com>
2025-08-30 22:51:44 +0200

    change border size

--- src/components/form/formCard/formCard.jsx
+++ src/components/form/formCard/formCard.jsx
@@ -7,7 +7,7 @@ export const FormCard = () => {
       <h2 className="form-card-title">Form Demo Application</h2>
       <p className="form-card-description">
         The Form Application supports mobile responsiveness and user-friendly validation.
-        It is optimized for Android and iOS devices, especially for Safari and Chrome browsers.
+        It is optimized for Android and iOS devices, especially for Chrome and Safari browsers.
       </p>
     </div>
   );
